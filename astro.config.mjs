c9500664b3e5986420a1227b921da026a75307fd
DinarPutra1408 <dinar.mubarok23@gmail.com>
2026-06-06 00:00:41 +0700

    feat: pwa resmi aktif dan siap install di hp jemaah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import { envField } from 'astro/config'; // Impor resmi Astro Env
-
 import tailwindcss from '@tailwindcss/vite';
+import AstroPWA from '@vite-pwa/astro'; // Impor fitur PWA
 
 export default defineConfig({
   output: 'server',
@@ -14,6 +14,37 @@ export default defineConfig({
     }
   },
 
+  // Taruh di astro.config.mjs kamu
+  integrations: [
+    AstroPWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // <-- TAMBAHKAN BARIS INI BIAR DI LOKAL GA 404 LAGI!
+      },
+      manifest: {
+        name: 'Absensi MM Tandes Barat',
+        short_name: 'AbsensiMM',
+        description: 'Aplikasi Absensi MM Desa Tandes Barat',
+        theme_color: '#090d16', 
+        background_color: '#090d16',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  
   // FIX DI SINI: 'env' ditaruh di root utama, BUKAN di dalam 'experimental' lagi!
   env: {
     schema: {
